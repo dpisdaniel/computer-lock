a8d11d8b86ae45f98686a9050b04e0123010acc9
User <User@COMP-24>
2017-02-09 17:45:29 +0200

    adding files

---
+++ ComputerLock/Trampoline/dllmain.cpp
@@ -0,0 +1,47 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+#include "mhook.h"
+
+
+typedef HGDIOBJ(WINAPI * _CreateFile) (LPCTSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+
+
+_CreateFile ActualCreateFile = (_CreateFile)GetProcAddress(GetModuleHandle(L"kernel32"), "CreateFileW");
+
+
+char* encode(const wchar_t* wstr, unsigned int codePage)
+{
+	int sizeNeeded = WideCharToMultiByte(codePage, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* encodedStr = new char[sizeNeeded];
+	WideCharToMultiByte(codePage, 0, wstr, -1, encodedStr, sizeNeeded, NULL, NULL);
+	return encodedStr;
+}
+
+HGDIOBJ WINAPI HookCreateFile(LPCTSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+{
+	char file_path[1024];
+	strcpy_s(file_path, encode(lpFileName, CP_UTF8)); //encode to utf-8
+	if (strstr(file_path, ".txt"))
+	{
+		return INVALID_HANDLE_VALUE;
+	}
+	return ActualCreateFile(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
+void AttachHook() {
+
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		AttachHook();
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
---
+++ ComputerLock/Trampoline/stdafx.h
@@ -0,0 +1,25 @@
+#pragma once
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <stdio.h>
+#include <shlwapi.h>
+#include <iostream>
+using namespace std;
+// Don't forget to include "Ws2_32.lib" in the library list.
+#include <winsock2.h> 
+#include <string.h>
+#include <iomanip>
+#include <assert.h>
+
+
+// TODO: reference additional headers your program requires here
