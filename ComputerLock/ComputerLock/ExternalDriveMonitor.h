66d5f7eabd395c0f3e6283741c2b432c12a04cf7
User <User@COMP-24>
2017-04-20 19:26:04 +0300

    added prototyping for ExternalDriveMonitor

--- ComputerLock/ComputerLock/ExternalDriveMonitor.h
+++ ComputerLock/ComputerLock/ExternalDriveMonitor.h
@@ -1,1 +1,22 @@
 #pragma once
+
+namespace LogicalDriveMonitor {
+	class LogicalDriveRetriever {
+	public:
+		//Class constructor
+		//It should store the current logical drives in the logicalDrives member
+		LogicalDriveRetriever();
+
+		void AddDriveMonitor(int driveLetter);
+
+	private:
+		vector<DWORD> logicalDrives;
+
+		void WaitForNewExternalDrives();
+
+		void RetrieveCurrentLogicalDrives();
+
+		/*Detects if the given logical drive is an external drive*/
+		void DetectExternalDrives(int driveLetter);
+	};
+}
