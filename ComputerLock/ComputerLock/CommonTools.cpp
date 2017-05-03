5bbfdfc2a59b9d4d7ed4134d43ce51e13f93f1cd
Daniel <daniel.pakula102@gmail.com>
2017-05-03 16:04:27 +0300

    Add common method interface

---
+++ ComputerLock/ComputerLock/CommonTools.cpp
@@ -0,0 +1,62 @@
+#include "stdafx.h"
+#include "CommonTools.h"
+
+string GetLastErrorAsString(DWORD errorMessageID)
+{
+	const string MTAG = ".GetLastErrorAsString";
+	//Get the error message, if any.
+	if (errorMessageID == 0)
+		return string(); //No error message has been recorded
+
+	LPSTR messageBuffer = nullptr;
+	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+	string message(messageBuffer, size);
+
+	//Free the buffer.
+	LocalFree(messageBuffer);
+
+	return message;
+}
+
+BOOL SetPrivilegeByToken(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege) {
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!LookupPrivilegeValue(NULL, lpszPrivilege, &luid))
+	{
+		cout << "LookupPrivilegeValue error: " << GetLastErrorAsString(GetLastError()) << endl;
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	if (bEnablePrivilege)
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	else
+		tp.Privileges[0].Attributes = 0;
+
+	// Enable the privilege or disable all privileges.
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL)) {
+		cout << "AdjustTokenPrivileges error: " << GetLastErrorAsString(GetLastError()) << endl;
+		return FALSE;
+	}
+
+	if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+		cout << "The token does not have the specified privilege" << endl;
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL SetPrivilegeByName(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege) {
+	HANDLE hProcessToken = NULL;
+	HANDLE hProcess = GetCurrentProcess();
+	if (!OpenProcessToken(hProcess, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hProcessToken)) {
+		cout << "Failed to retrieve this process's access token. error:" << GetLastErrorAsString(GetLastError()) << endl;
+		return FALSE;
+	}
+	return SetPrivilegeByToken(hProcessToken, lpszPrivilege, bEnablePrivilege);
+}
---
+++ ComputerLock/ComputerLock/CommonTools.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/*This is where methods that are used but don't really belong anywhere are put*/
+
+//Turns the GetLastError method's output into a printable std::string
+string GetLastErrorAsString(DWORD errorMessageID);
+
+/*Sets a given privilege for the access token handle given according to the name of the privilege.
+Enables or disables the privilege if bEnablePrivilege is True or False respectively*/
+BOOL SetPrivilegeByToken(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
+
+/*Enables or disables the privilege given by the privilege name in lpszPrivilege. if bEnablePrivilege is True, enables the privilege,
+else disables it. This method will set the given privilege for the current process's access token.
+To set a privilege for a different access token use SetPrivilegeByToken*/
+BOOL SetPrivilegeByName(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
+
+/*Compares the magic numbers of a file and a string representation of the magic number. Returns
+TRUE if the magic numbers are the same and FALSE otherwise*/
+BOOL CompareFileMagic(HANDLE hFile, char* magicStr)
